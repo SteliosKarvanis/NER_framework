e0a4b057812d2029786cbab8c561d8ce5b1d5bcd
stelios <stelioscbranco@hotmail.com>
2023-09-20 11:21:17 -0300

    letting generic configs

--- configs.py
+++ configs.py
@@ -1,18 +1,6 @@
-LABELS = [
-    "O",
-    "B-PRO",
-    "B-MAR",
-    "B-ESP",
-    "B-TAM",
-    "B-QUA",
-    "I-PRO",
-    "I-MAR",
-    "I-ESP",
-    "I-TAM",
-    "I-QUA",
-]
-TRAIN_ITER=200
+TRAIN_ITER = 200
 DROPOUT_RATE = 0.5
 DATA_DIR = 'data'
 MODEL_NAME = "pt_core_news_lg"
 MODEL_OUTPUT_DIR = 'ner_v0'
+LABELS = []
